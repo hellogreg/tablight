d82e3cf489562ba5adde25eecd4457abc5b125df
hellogreg <booksmart@gmail.com>
2015-08-30 15:57:08 -0500

    Injected all CSS via JavaScript, rather than the .css file, though that still can be used.

--- tablight.js
+++ tablight.js
@@ -2,12 +2,6 @@
 
   "use strict";
 
-  // TODO: Instead of having a separate CSS file, inject and apply the required CSS styles with this JS file
-  // Resources...
-  // http://davidwalsh.name/add-rules-stylesheets
-  // http://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
-  // http://stackoverflow.com/questions/1720320/how-to-dynamically-create-css-class-in-javascript-and-apply
-
   function tablight() {
 
     // Document elements and element arrays, prefixed with $.
@@ -19,12 +13,46 @@
     // Max # of pixels for what we consider a non-desktop, non-tablet mobile device.
     var maxMobileWidth = 600;
 
+    // Instead of having a separate CSS file, inject and apply the required CSS styles with this JS file
+    // I'd still use the CSS file for myself, but for public use, might be better if all were in one file.
+    // If you'd rather use tablight.css, don't call this function and uncomment the CSS in that file.
+    //
+    // Resources...
+    // http://davidwalsh.name/add-rules-stylesheets
+    // http://yuiblog.com/blog/2007/06/07/style/
+    // http://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
+    // http://stackoverflow.com/questions/1720320/how-to-dynamically-create-css-class-in-javascript-and-apply
+    function injectDocumentStyles () {
+      var styleElement = document.createElement("style");
+      styleElement.type = "text/css";
+
+      var styles = [];
+
+      styles.push("@media screen and (min-width: " + (maxMobileWidth + 1) + "px) {");
+      styles.push(".hidden-tab { display: none; }");
+      styles.push(".tab-nav ul {border-bottom: 1px solid #ddd;font-family: sans-serif;font-size: .625rem;line-height: 1.25rem;list-style: none;padding: 0;text-transform: uppercase;}");
+      styles.push(".tab-nav ul li {display: inline;margin-right: .25rem;}");
+      styles.push(".tab-nav .tab-button {background: #eee;border-radius: .25rem .25rem 0 0;color: #555;padding: .25rem .375rem;text-decoration: none;}");
+      styles.push("@-moz-document url-prefix() {.tab-nav .tab-button {padding: .3rem .375rem;}}");
+      styles.push(".tab-nav .tab-button.selected {background: #ddd;color: #444;}");
+      styles.push(".tab-nav .tab-button:hover {background: #ccc;color: #333;}");
+      styles.push(".tab {border-bottom: 1px solid #ddd;padding: 1rem;}");
+      styles.push("}");
+
+      var stylesheet = styles.join("\n");
+
+      console.log(stylesheet);
+
+      styleElement.appendChild(document.createTextNode(stylesheet));
+      document.head.appendChild(styleElement);
+    }
+
     // Hide all tabs, unless a param is sent to ignore the first one (e.g., on page load).
     function hideTabs(showFirstTab) {
       var i = showFirstTab ? 1 : 0;
       var len = $allTabs.length;
       for (; i < len; i++) {
-        $allTabs[i].classList.add("invisible");
+        $allTabs[i].classList.add("hidden-tab");
       }
     }
 
@@ -50,7 +78,7 @@
           e.preventDefault();
           hideTabs();
           deselectAllTabButtons();
-          $targetTab.classList.remove("invisible");
+          $targetTab.classList.remove("hidden-tab");
           $target.classList.add("selected");
         }
       }, false);
@@ -62,6 +90,7 @@
       var showFirstTab = true;
       if (tabButtonLen > 0) {
 
+        injectDocumentStyles();
         $allTabButtons[0].classList.add("selected");
         hideTabs(showFirstTab);
 
